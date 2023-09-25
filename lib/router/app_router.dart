1747ba0ca8ee0c7bc4f6e375701274fea110b152
sobhan ziyaei <sobhan.ziaee73@gmail.com>
2023-09-25 12:12:05 +0330

    create structure of app

---
+++ lib/router/app_router.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      //case :
+
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/ui/constants/colors.dart
@@ -0,0 +1,4 @@
+
+import 'package:flutter/material.dart';
+
+const bgColor = Color.fromARGB(0, 184, 184, 185);
